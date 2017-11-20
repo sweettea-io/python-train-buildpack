c3f9e439205328dfd532cd4da1171645ef605a09
Ben Whittle <benwhittle31@gmail.com>
2017-11-19 19:49:40 -0500

    logging for debugging

--- src/uploader.py
+++ src/uploader.py
@@ -5,4 +5,7 @@ s3 = boto3.resource('s3')
 
 def upload(filepath=None, upload_path=None, bucket=None):
   bucket = s3.Bucket(bucket)
+
+  print filepath
+
   bucket.upload_file(filepath, upload_path)
