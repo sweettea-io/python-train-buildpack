123481a9e391bae965166c2bd27035c5253f52eb
Ben Whittle <benwhittle31@gmail.com>
2017-11-19 19:20:42 -0500

    upload_fileobj --> upload_file

--- src/uploader.py
+++ src/uploader.py
@@ -5,6 +5,4 @@ s3 = boto3.resource('s3')
 
 def upload(filepath=None, upload_path=None, bucket=None):
   bucket = s3.Bucket(bucket)
-
-  with open(filepath, 'rb') as data:
-    bucket.upload_fileobj(data, upload_path)
+  bucket.upload_file(filepath, upload_path)
