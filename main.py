874cfd4743a4cfa8715f5dd74e2d00e42e356a3f
Ben Whittle <benwhittle31@gmail.com>
2017-12-10 14:56:27 -0800

    logging to redis

--- main.py
+++ main.py
@@ -15,11 +15,10 @@ def get_envs():
     'DATASET_TABLE_NAME': False,
     'CORE_URL': False,
     'CORE_API_TOKEN': False,
-    'TEAM': True,
-    'TEAM_UID': True,
     'PREDICTION': True,
     'PREDICTION_UID': True,
-    'DEPLOYMENT_UID': True
+    'DEPLOYMENT_UID': True,
+    'REDIS_URL': False
   }
 
   missing = [k for k in required_envs if k not in os.environ]
@@ -32,32 +31,32 @@ def get_envs():
   return {k.lower(): os.environ.get(k) for k in required_envs if required_envs[k]}
 
 
-def read_config(config_path):
+def read_config(config_path, logger=None):
   with open(config_path) as f:
     config = yaml.load(f)
   
   if type(config) != dict or 'train' not in config or 'model' not in config:
-    print('Not training. Invalid config file: {}'.format(config))
+    logger.log('Not training. Invalid config file: {}'.format(config))
     exit(1)
   
   return config
 
 
-def get_exported_method(config, key=None):
+def get_exported_method(config, key=None, logger=None):
   module_str, function_str = config.get(key).split(':')
 
   if not module_str:
-    print('No module specified for config method({}={}).'.format(key, config.get(key)))
+    logger.log('No module specified for config method({}={}).'.format(key, config.get(key)))
     exit(1)
 
   module = importlib.import_module(module_str)
 
   if not module:
-    print('No module to import at destination: {}'.format(module_str))
+    logger.log('No module to import at destination: {}'.format(module_str))
     exit(1)
 
   if not hasattr(module, function_str):
-    print('No function named {} exists on module {}'.format(function_str, module_str))
+    logger.log('No function named {} exists on module {}'.format(function_str, module_str))
     exit(1)
 
   return getattr(module, function_str)
@@ -67,7 +66,7 @@ def get_src_mod(src, name):
   return importlib.import_module('{}.{}'.format(src, name))
 
 
-def create_dataset(config):
+def create_dataset(config, logger=None):
   create_dataset_method = get_exported_method(config, key='create_dataset')
   db_url = os.environ.get('DATASET_DB_URL')
   table_name = os.environ.get('DATASET_TABLE_NAME')
@@ -77,55 +76,54 @@ def create_dataset(config):
     engine = create_engine(db_url)
     conn = engine.connect()
   except BaseException as e:
-    print('Error connecting to DATASET_DB_URL {} when attempting to create dataset: {}'.format(db_url, e))
+    logger.log('Error connecting to DATASET_DB_URL {} when attempting to create dataset: {}'.format(db_url, e))
     exit(1)
 
   try:
     # TODO: Prevent SQL Injection here
     data = [r for r in conn.execute('SELECT data FROM {}'.format(table_name))]
   except BaseException as e:
-    print('Error querying all records for DB: {}, with error: {}'.format(db_url, e))
+    logger.log('Error querying all records for DB: {}, with error: {}'.format(db_url, e))
     exit(1)
 
   # Call the provided method and pass in the data
   create_dataset_method(data)
 
 
-def perform(team=None, team_uid=None, prediction=None,
-            prediction_uid=None, s3_bucket_name=None, deployment_uid=None):
+def perform(prediction=None, prediction_uid=None, s3_bucket_name=None, deployment_uid=None):
   # We need to use importlib.import_module to access our src/ files since src/ will
   # be renamed to <prediction_uid>/ to avoid conflicts with user's project files
 
   # Get refs to the modules inside our src directory
-  print('Importing modules...')
+  logger = get_src_mod(prediction_uid, 'logger')
+  logger.log('Importing modules...')
   uploader = get_src_mod(prediction_uid, 'uploader')
   definitions = get_src_mod(prediction_uid, 'definitions')
   messenger = get_src_mod(prediction_uid, 'messenger')
 
   # Read the config file in the project
-  print('Validating config...')
-  config_path = getattr(definitions, 'config_path')
-  config = read_config(config_path)
+  logger.log('Validating {}...'.format(definitions.config_file))
+  config = read_config(definitions.config_path, logger=logger)
 
   # Fetch all data from DB and pass it into the specified create_dataset method
-  print('Pulling data from dataset DB...')
-  # create_dataset(config)
+  logger.log('Pulling data from dataset DB...')
+  # create_dataset(config, logger=logger)
 
   # Get ref to exported train method and execute it
-  print('Executing train method...')
-  train_method = get_exported_method(config, key='train')
+  logger.log('Executing train method...')
+  train_method = get_exported_method(config, key='train', logger=logger)
   train_method()
 
   # If test method specified, call that as well
   if config.get('test'):
-    print('Executing test method...')
-    test_method = get_exported_method(config, key='test')
+    logger.log('Executing test method...')
+    test_method = get_exported_method(config, key='test', logger=logger)
     test_method()
 
   # Get trained model path and proper file ext before uploading it to S3
   model_path = config.get('model')
 
-  print('Finding trained model at path {}'.format(model_path))
+  logger.log('Finding trained model at path {}'.format(model_path))
 
   if '.' in model_path:
     model_file_type = model_path.split('.').pop()
@@ -137,16 +135,16 @@ def perform(team=None, team_uid=None, prediction=None,
   abs_model_path = os.path.abspath(os.path.join(os.path.dirname(__file__), model_path))
 
   if not os.path.exists(abs_model_path):
-    print('No trained model at path {}. Not uploading model.'.format(model_path))
+    logger.log('No trained model at path {}. Not uploading model.'.format(model_path))
     exit(1)
 
   # Upload trained model to S3
-  print('Uploading trained model...')
+  logger.log('Uploading trained model...')
   uploader.upload(filepath=abs_model_path,
                   upload_path=upload_path,
                   bucket=s3_bucket_name)
 
-  print('DONE TRAINING.')
+  logger.log('Done training.')
 
   # Tell Core we're done training
   messenger.send_message({
--- src/definitions.py
+++ src/definitions.py
@@ -2,7 +2,8 @@ import os
 
 base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
-config_path = base_dir + '/.tensorci.yml'
+config_file = '.tensorci.yml'
+config_path = base_dir + '/' + config_file
 
 TRAIN_CLUSTER = 'train'
 API_CLUSTER = 'api'
