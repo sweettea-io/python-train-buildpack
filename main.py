96ade4026b14c3d95541e2d6e189b5985df04942
Ben Whittle <benwhittle31@gmail.com>
2018-02-24 23:10:42 -0800

    moving envs into own Envs class

--- main.py
+++ main.py
@@ -6,34 +6,6 @@ import sys
 from sqlalchemy import create_engine
 
 
-def get_envs():
-  # {key: <is_param>}
-  required_envs = {
-    'AWS_ACCESS_KEY_ID': False,
-    'AWS_SECRET_ACCESS_KEY': False,
-    'AWS_REGION_NAME': False,
-    'S3_BUCKET_NAME': True,
-    'DATASET_DB_URL': False,
-    'DATASET_TABLE_NAME': False,
-    'CORE_URL': False,
-    'CORE_API_TOKEN': False,
-    'REPO_SLUG': True,
-    'REPO_UID': True,
-    'DEPLOYMENT_UID': True,
-    'REDIS_URL': False,
-    'UPDATE_PREDICTION_MODEL': True
-  }
-
-  missing = [k for k in required_envs if k not in os.environ]
-
-  if missing:
-    print('Not training. The following env vars not provided: {}'.format(', '.join(missing)))
-    exit(1)
-
-  # Only return the envs that we need as params
-  return {k.lower(): os.environ.get(k) for k in required_envs if required_envs[k]}
-
-
 def read_config(config_path):
   with open(config_path) as f:
     config = yaml.load(f)
@@ -65,26 +37,24 @@ def get_exported_method(config, key=None):
   return getattr(module, function_str)
 
 
-def get_src_mod(src, name):
-  return importlib.import_module('{}.{}'.format(src, name))
+def get_src_mod(name):
+  return importlib.import_module('{}.{}'.format(os.environ.get('REPO_UID'), name))
 
 
-def prepro_data(prepro_method, table_name, log_capture, log_queue):
-  db_url = os.environ.get('DATASET_DB_URL')
-
+def prepro_data(prepro_method, envs, log_capture, log_queue):
   try:
     # Connect to database holding dataset records
-    engine = create_engine(db_url)
+    engine = create_engine(envs.DATASET_DB_URL)
   except BaseException as e:
-    print('Error connecting to DATASET_DB_URL {} -- {}'.format(db_url, e))
+    print('Error connecting to DATASET_DB_URL: {}, with error {}.'.format(envs.DATASET_DB_URL, e))
     exit(1)
 
   try:
     # Get all JSON data records
     print('Extracting dataset...')
-    data = [r[0] for r in engine.execute('SELECT data FROM {};'.format(table_name))]
+    data = [r[0] for r in engine.execute('SELECT data FROM {};'.format(envs.DATASET_TABLE_NAME))]
   except BaseException as e:
-    print('Error querying dataset data (db_url={}) -- {}'.format(db_url, e))
+    print('Error querying dataset data (db_url={}): {}.'.format(envs.DATASET_DB_URL, e))
     exit(1)
 
   call_exported_method(log_capture, log_queue, 'prepro_data', prepro_method, data)
@@ -141,64 +111,52 @@ def get_model_file_info(model_path, repo_slug):
   return path_to_upload, model_ext, upload_path
 
 
-def perform(repo_slug=None, repo_uid=None, s3_bucket_name=None,
-            deployment_uid=None, update_prediction_model=None):
-  # Get refs to the modules inside our src directory
-  print('Importing modules...')
-  uploader = get_src_mod(repo_uid, 'uploader')
-  definitions = get_src_mod(repo_uid, 'definitions')
-  messenger = get_src_mod(repo_uid, 'messenger')
-  redis = get_src_mod(repo_uid, 'pyredis')
+def perform():
+  # Get exported internal src modules
+  envs = get_src_mod('envs').envs
+  uploader = get_src_mod('uploader')
+  definitions = get_src_mod('definitions')
+  messenger = get_src_mod('messenger')
+  redis = get_src_mod('pyredis')
 
   # Read the config file in the project
-  print('Validating {}...'.format(definitions.config_file))
   config = read_config(definitions.config_path)
 
   # Define our log redirects
   log_capture = redis.RedisStream
-  log_queue = 'train:{}'.format(deployment_uid)
-
-  # dataset-db table name to pull dataset from
-  table_name = os.environ.get('DATASET_TABLE_NAME')
+  log_queue = 'train:{}'.format(envs.DEPLOYMENT_UID)
 
   # Exported method that preprocesses dataset before training
   prepro_data_method = get_exported_method(config, key='prepro_data')
 
   # Only preprocess dataset if a table name was provided.
-  if table_name and prepro_data_method:
-    # Retrieve dataset from DB and pass it into the specified prepro_data method
-    prepro_data(prepro_data_method, table_name, log_capture, log_queue)
+  if envs.DATASET_TABLE_NAME and prepro_data_method:
+    prepro_data(prepro_data_method, envs, log_capture, log_queue)
 
   # Get ref to exported train method and execute it
-  print('Executing train method...')
   train_method = get_exported_method(config, key='train')
   call_exported_method(log_capture, log_queue, 'train', train_method)
 
   # If test method specified, call that as well
   if config.get('test'):
-    print('Executing test method...')
     test_method = get_exported_method(config, key='test')
     call_exported_method(log_capture, log_queue, 'test', test_method)
 
   # Get trained model path and proper file ext before uploading it to S3
-  local_model_path, model_ext, upload_path = get_model_file_info(config.get('model'), repo_slug)
+  local_model_path, model_ext, upload_path = get_model_file_info(config.get('model'), envs.REPO_SLUG)
 
   # Upload trained model to S3
-  print('Uploading trained model...')
   uploader.upload(filepath=local_model_path,
                   upload_path=upload_path,
-                  bucket=s3_bucket_name)
-
-  print('Done training.')
+                  bucket=envs.S3_BUCKET_NAME)
 
   # Tell Core we're done training
   messenger.report_done_training({
-    'deployment_uid': deployment_uid,
-    'update_prediction_model': update_prediction_model == 'true',
+    'deployment_uid': envs.DEPLOYMENT_UID,
+    'update_prediction_model': envs.UPDATE_PREDICTION_MODEL == 'true',
     'model_ext': model_ext
   })
 
 
 if __name__ == '__main__':
-  params = get_envs()
-  perform(**params)
+  perform()
