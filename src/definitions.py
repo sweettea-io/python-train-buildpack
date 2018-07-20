ee31d659a700ae25b7a77dc547e4123eb368a0f7
Ben Whittle <benwhittle31@gmail.com>
2018-07-20 01:46:44 -0700

    change config file def to ".sweettea.yml"

--- src/definitions.py
+++ src/definitions.py
@@ -2,7 +2,7 @@ import os
 
 base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
-config_file = '.tensorci.yml'
+config_file = '.sweettea.yml'
 config_path = base_dir + '/' + config_file
 
 TRAIN_CLUSTER = 'train'
